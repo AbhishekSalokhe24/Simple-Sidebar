60389a409941351214cfe84e7f47d0d1deb8fe90
AbhishekSalokhe24 <anishsalokhe1111@gmail.com>
2024-07-07 15:04:44 +0530

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { SidebarComponent } from './Components/sidebar/sidebar.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    
+];
